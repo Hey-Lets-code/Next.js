d9636b1ec0977c85c78c16e175e7908c19be4512
Leticia <le.augusta.souza@gmail.com>
2024-09-02 00:14:53 -0300

    fix: deleted tailwind.js file, tailwind is working now.

--- NextJs/reddit/src/app/page.tsx
+++ NextJs/reddit/src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/react";
 import * as actions from "@/actions";
 import { auth } from "@/auth";
 import Profile from "@/components/profile";
+import "./globals.css";
 
 export default async function Home() {
   const session = await auth();
