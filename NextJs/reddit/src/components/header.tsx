7e7d116cdaf076f99327053263b893cced1527aa
Leticia <le.augusta.souza@gmail.com>
2024-09-03 23:55:32 -0300

    Autenticação

--- NextJs/reddit/src/components/header.tsx
+++ NextJs/reddit/src/components/header.tsx
@@ -5,40 +5,10 @@ import {
   NavbarContent,
   NavbarItem,
   Input,
-  Button,
-  Avatar,
 } from "@nextui-org/react";
-import { auth } from "@/auth";
-import * as actions from "@/actions";
+import HeaderAuth from "@/components/header-auth";
 
-export default async function Header() {
-  const session = await auth();
-
-  let authContent: React.ReactNode;
-  if (session?.user) {
-    authContent = <Avatar src={session.user.image || ""} />;
-  } else {
-    authContent = (
-      <>
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="secondary" variant="bordered">
-              Sign In
-            </Button>
-          </form>
-        </NavbarItem>
-
-        <NavbarItem>
-          <form action={actions.signIn}>
-            <Button type="submit" color="primary" variant="flat">
-              Sign Up
-            </Button>
-          </form>
-        </NavbarItem>
-      </>
-    );
-  }
-
+export default function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
@@ -50,7 +20,9 @@ export default async function Header() {
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end">{authContent}</NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
     </Navbar>
   );
 }
